541730639e9850fc043ed8553fdaaaff52867441
Yuda <yuda10aditya@gmail.com>
2021-06-15 11:33:34 +0700

    demo-dnd

---
+++ src/App.js
@@ -0,0 +1,121 @@
+import React, { useState } from "react";
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
+import DragIndicatorIcon from "@material-ui/icons/DragIndicator";
+import Grid from "@material-ui/core/Grid";
+import Button from "@material-ui/core/Button";
+import ArrowRightAltIcon from "@material-ui/icons/ArrowRightAlt";
+import "./App.css";
+
+const finalSpaceCharacters = [
+  {
+    id: "1",
+    name: "Saya menyukai menjadi orang yang mengambil keputusan dan tanggung jawab",
+    label: "Sangat sesuai",
+    thumb: "/images/gary.png",
+  },
+  {
+    id: "2",
+    name: "Saya menyukai pekerjaan yang melibatkan aktivitas fisik",
+    label: "Sesuai",
+    thumb: "/images/cato.png",
+  },
+  {
+    id: "3",
+    name: "Saya peka terhadap perasaan orang lain asdasdasd ",
+    label: "Agak sesuai",
+    thumb: "/images/kvn.png",
+  },
+  {
+    id: "4",
+    name: "Saya senang mencoba tugas yang berhubungan dengan pekerjaan baru",
+    label: "Agak tidak sesuai",
+    thumb: "/images/mooncake.png",
+  },
+  {
+    id: "5",
+    name: "Saya menyukai menjadi orang yang mengambil keputusan dan tanggung jawab",
+    label: "Tidak sesuai",
+    thumb: "/images/quinn.png",
+  },
+  {
+    id: "6",
+    name: "Banyak orang menganggap saya adalah orang yang selalu ingin tahu",
+    label: "Sangat tidak sesuai",
+    thumb: "/images/quinn.png",
+  },
+];
+
+function App() {
+  const [characters, updateCharacters] = useState(finalSpaceCharacters);
+
+  function handleOnDragEnd(result) {
+    if (!result.destination) return;
+
+    const items = Array.from(characters);
+    const [reorderedItem] = items.splice(result.source.index, 1);
+    items.splice(result.destination.index, 0, reorderedItem);
+
+    updateCharacters(items);
+  }
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>RIASEC</h1>
+        <div className="container">
+          <div className="number-list">
+            <p>1</p>
+            <p>2</p>
+            <p>3</p>
+            <p>4</p>
+            <p>5</p>
+            <p>6</p>
+          </div>
+          <DragDropContext onDragEnd={handleOnDragEnd}>
+            <Droppable droppableId="characters">
+              {(provided) => (
+                <ul
+                  className="characters"
+                  {...provided.droppableProps}
+                  ref={provided.innerRef}
+                >
+                  <div>
+                    {characters.map(({ id, name, label, thumb }, index) => {
+                      return (
+                        <Draggable key={id} draggableId={id} index={index}>
+                          {(provided) => (
+                            <li
+                              ref={provided.innerRef}
+                              {...provided.draggableProps}
+                              {...provided.dragHandleProps}
+                            >
+                              {/* <label className="label">{label}</label> */}
+                              <div className={`border${index}`}></div>
+                              <p>
+                                {name}
+                                <DragIndicatorIcon />
+                              </p>
+                            </li>
+                          )}
+                        </Draggable>
+                      );
+                    })}
+                    {provided.placeholder}
+                  </div>
+                </ul>
+              )}
+            </Droppable>
+          </DragDropContext>
+        </div>
+      </header>
+      <Button variant="contained" color="primary" className="button-next">
+        <ArrowRightAltIcon />
+      </Button>
+      <p>
+        <a href="https://www.talentlytica.com/">Talentlytica</a>
+      </p>
+    </div>
+  );
+}
+
+export default App;
