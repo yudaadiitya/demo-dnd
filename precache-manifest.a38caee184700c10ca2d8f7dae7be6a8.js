d5ba4f2efa7ef74814a1f30e8727d726afa53fd2
Yuda <yuda10aditya@gmail.com>
2021-06-15 11:35:13 +0700

    Updates

---
+++ precache-manifest.a38caee184700c10ca2d8f7dae7be6a8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "036f2de5a043cf4dd20f3072078054b8",
+    "url": "/demo-dnd/index.html"
+  },
+  {
+    "revision": "c661d720d447fe604ef9",
+    "url": "/demo-dnd/static/css/main.fad2c720.chunk.css"
+  },
+  {
+    "revision": "de679e95da262c82d130",
+    "url": "/demo-dnd/static/js/2.fafd3ee8.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/demo-dnd/static/js/2.fafd3ee8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c661d720d447fe604ef9",
+    "url": "/demo-dnd/static/js/main.20749cab.chunk.js"
+  },
+  {
+    "revision": "a5a9a6784cc9ae4acc95",
+    "url": "/demo-dnd/static/js/runtime-main.d5ecec3f.js"
+  }
+]);
